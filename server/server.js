e81967bce653837bd60175295c7465938ae25a17
Vishesh Mishra <visheshmishra16@gmail.com>
2019-01-09 22:36:10 +0530

    =port edited and package also

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const ObjectID = require('mongodb')
 const express = require('express');
 const  bodyParser = require('body-parser');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
